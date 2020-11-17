80c7f235af2283979196929c8e6906bddd30b105
meuequalsD <nirmalkrishnav@gmail.com>
2020-11-17 08:43:23 +0530

    changes to state

--- src/components/generator/Generator.js
+++ src/components/generator/Generator.js
@@ -3,20 +3,27 @@ import GeneratorCore from './GeneratorCore/GeneratorCore';
 
 const Generator = (props) => {
 
-    const [nameOne, setNameOne] = useState('');
-    const [nameTwo, setNameTwo] = useState('');
-    const [year, setYear] = useState('');
+    const [nameOne, setNameOne] = useState('John');
+    const [nameTwo, setNameTwo] = useState('Jill');
+    const [year, setYear] = useState(() => {
+        let d = new Date();
+        return d.getFullYear().toString();
+    });
 
     const sentenseCase = (val) => {
+        if (val !== null && val.length < 1) {
+            return ''
+        }
+
         val = val[0].toUpperCase() + val.substring(1);
         return val;
     }
 
     return (
         <div>
-            <input name="nameOne" onChange={e => setNameOne(sentenseCase(e.target.value))} />
-            <input name="nameTwo" onChange={e => setNameTwo(sentenseCase(e.target.value))} />
-            <input name="year" onChange={e => setYear(e.target.value)} />
+            <input value={nameOne} name="nameOne" onChange={e => setNameOne(sentenseCase(e.target.value))} />
+            <input value={nameTwo} name="nameTwo" onChange={e => setNameTwo(sentenseCase(e.target.value))} />
+            <input value={year} name="year" onChange={e => setYear(e.target.value)} />
 
             <GeneratorCore nameOne={nameOne} nameTwo={nameTwo} year={year} />
         </div>
